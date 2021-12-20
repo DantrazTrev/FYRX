03599af3de4f501ebbaa6ea874d6300ffc1ef1e6
Ayush Pratap Singh <ayushpratap16@gmail.com>
2021-12-20 13:09:10 +0530

    Deploy2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React from 'react';
 import Home from './pages/Home';
 import { Animate } from './utils/animation';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Cam from './pages/Cam';
 function App() {
   Animate();
   return (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './index.css';
-import { Random } from '../../utils/animation';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
